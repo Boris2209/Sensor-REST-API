5adb1e9f89e5368566b42c10ab5bb60fe8021336
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-07 10:57:46 +0300

    Init service

---
+++ src/main/java/ru/boris/springboot/Sensor/REST/API/services/SensorService.java
@@ -0,0 +1,23 @@
+package ru.boris.springboot.Sensor.REST.API.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.boris.springboot.Sensor.REST.API.models.Sensor;
+import ru.boris.springboot.Sensor.REST.API.repositories.SensorRepositopy;
+
+@Service
+@Transactional(readOnly = true)
+public class SensorService {
+    private final SensorRepositopy sensorRepositopy;
+
+    @Autowired
+    public SensorService(SensorRepositopy sensorRepositopy) {
+        this.sensorRepositopy = sensorRepositopy;
+    }
+
+    @Transactional
+    public void save(Sensor sensor) {
+        sensorRepositopy.save(sensor);
+    }
+}
