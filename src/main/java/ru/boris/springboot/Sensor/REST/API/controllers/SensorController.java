acd5bb504b1a1846e020e592c534286750e49667
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-07 11:28:12 +0300

    Added method POST for created new sensors

--- src/main/java/ru/boris/springboot/Sensor/REST/API/controllers/SensorController.java
+++ src/main/java/ru/boris/springboot/Sensor/REST/API/controllers/SensorController.java
@@ -1,13 +1,20 @@
 package ru.boris.springboot.Sensor.REST.API.controllers;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import ru.boris.springboot.Sensor.REST.API.dto.SensorDTO;
+import ru.boris.springboot.Sensor.REST.API.models.Sensor;
 import ru.boris.springboot.Sensor.REST.API.services.SensorService;
+import ru.boris.springboot.Sensor.REST.API.util.ErrorResponse;
+import ru.boris.springboot.Sensor.REST.API.util.SensorNotCreatedException;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/sensors")
@@ -15,16 +22,42 @@ public class SensorController {
 
     private final SensorService sensorService;
 
+    private final ModelMapper modelMapper;
+
     @Autowired
-    public SensorController(SensorService sensorService) {
+    public SensorController(SensorService sensorService, ModelMapper modelMapper) {
         this.sensorService = sensorService;
+        this.modelMapper = modelMapper;
     }
 
     @PostMapping("/registration")
-    public ResponseEntity<HttpStatus> registration() {
+    public ResponseEntity<HttpStatus> registration(@RequestBody @Valid SensorDTO sensorDTO,
+                                                   BindingResult bindingResult) {
 
+        if (bindingResult.hasErrors()) {
+            StringBuilder errorsMsg = new StringBuilder();
+            List<FieldError> errors = bindingResult.getFieldErrors();
 
+            for (FieldError error : errors) {
+                errorsMsg.append(error.getField()).append(" - ").append(error.getDefaultMessage());
+            }
+
+            throw new SensorNotCreatedException(errorsMsg.toString());
+        }
+
+        sensorService.save(convertToSensor(sensorDTO));
 
         return ResponseEntity.ok(HttpStatus.OK);
+    }
+
+    private Sensor convertToSensor(SensorDTO sensorDTO) {
+        return modelMapper.map(sensorDTO, Sensor.class);
+    }
+
+    @ExceptionHandler
+    private ResponseEntity<ErrorResponse> handleException(SensorNotCreatedException e) {
+        ErrorResponse errorResponse = new ErrorResponse();
+        errorResponse.setMessage(e.getMessage());
+        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
     }
 }
