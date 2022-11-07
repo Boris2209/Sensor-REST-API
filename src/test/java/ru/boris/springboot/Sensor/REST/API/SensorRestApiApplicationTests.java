7e485a18c57e8b029f6f57b43059b58aca7866f0
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-07 08:57:07 +0300

    Initial commit

---
+++ src/test/java/ru/boris/springboot/Sensor/REST/API/SensorRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.boris.springboot.Sensor.REST.API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SensorRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
