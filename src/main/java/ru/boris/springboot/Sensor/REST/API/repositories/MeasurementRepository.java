a6078b78aece640406e593b43f9962d5cbc0afb8
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-07 10:57:29 +0300

    Added annotations

--- src/main/java/ru/boris/springboot/Sensor/REST/API/repositories/MeasurementRepository.java
+++ src/main/java/ru/boris/springboot/Sensor/REST/API/repositories/MeasurementRepository.java
@@ -1,7 +1,9 @@
 package ru.boris.springboot.Sensor.REST.API.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.boris.springboot.Sensor.REST.API.models.Measurement;
 
+@Repository
 public interface MeasurementRepository extends JpaRepository<Measurement, Integer> {
 }
--- src/main/java/ru/boris/springboot/Sensor/REST/API/repositories/SensorRepositopy.java
+++ src/main/java/ru/boris/springboot/Sensor/REST/API/repositories/SensorRepositopy.java
@@ -1,7 +1,9 @@
 package ru.boris.springboot.Sensor.REST.API.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.boris.springboot.Sensor.REST.API.models.Sensor;
 
+@Repository
 public interface SensorRepositopy extends JpaRepository<Sensor, Integer> {
 }
