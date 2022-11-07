87de12df8ef81c2b2ea616b19c4cbdf7a307d7f5
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-07 09:49:14 +0300

    Added model "Sensor"

---
+++ src/main/java/ru/boris/springboot/Sensor/REST/API/models/Sensor.java
@@ -0,0 +1,28 @@
+package ru.boris.springboot.Sensor.REST.API.models;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "Sensor")
+public class Sensor {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 3, max = 30, message = "Name should be between 2 and 30 character")
+    private String name;
+}
