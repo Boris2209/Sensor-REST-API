4dca282e05683d1c09c3e674f94618509333a5c7
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-07 11:27:32 +0300

    Init

---
+++ src/main/java/ru/boris/springboot/Sensor/REST/API/util/ErrorResponse.java
@@ -0,0 +1,12 @@
+package ru.boris.springboot.Sensor.REST.API.util;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class ErrorResponse {
+    private String message;
+}
