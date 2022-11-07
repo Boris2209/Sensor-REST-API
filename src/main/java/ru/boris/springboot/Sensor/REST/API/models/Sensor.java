35933d56ed37b5fa369400c0eae3a33c6e535872
Boris Litvinenko <boris.litvinenko@bk.ru>
2022-11-07 12:16:51 +0300

    2 -> 3

--- src/main/java/ru/boris/springboot/Sensor/REST/API/models/Sensor.java
+++ src/main/java/ru/boris/springboot/Sensor/REST/API/models/Sensor.java
@@ -25,7 +25,7 @@ public class Sensor {
 
     @Column(name = "name")
     @NotEmpty(message = "Name should not be empty")
-    @Size(min = 3, max = 30, message = "Name should be between 2 and 30 character")
+    @Size(min = 3, max = 30, message = "Name should be between 3 and 30 character")
     private String name;
 
     @OneToMany(mappedBy = "sensor")
